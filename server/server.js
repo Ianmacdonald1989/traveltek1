e99986dbf5c48f142a8e175d80832cf7f276d95c
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2024-02-26 13:04:47 +0000

    explaination comments for server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
 //creating MongoDB database 
 
-const express = require('express');
-const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
-const cors = require('cors')
+const express = require('express'); //imports Express.js framework 
+const app = express(); //creates instances 
+const MongoClient = require('mongodb').MongoClient; //connects mongo client to mongodb 
+const createRouter = require('./helpers/create_router.js');//creates router 
+const cors = require('cors') //enables cross-origin resource sharing 
 
-app.use(express.json());
-app.use(cors())
+app.use(express.json()); //parses requests with JSON file 
+app.use(cors())// allows different origin requests 
 
-MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true }) // connects to mongoDB 
 .then((client) => {
-const db = client.db('travel_hub');
-const flightCollection = db.collection('flights');
-const flightRouter = createRouter(flightCollection);
-app.use('/api/flights', flightRouter);
+const db = client.db('travel_hub'); //access to db 
+const flightCollection = db.collection('flights'); //reference to flights
+const flightRouter = createRouter(flightCollection); //using function 
+app.use('/api/flights', flightRouter); //handles requests to router path 
 
 })
-.catch(console.error);
+.catch(console.error); //handles errors 
 
 
 app.listen(9000, function () {
 console.log(`Listening on port ${ this.address().port }`);
-});
+}); //logs message if server is listening on port 9000
